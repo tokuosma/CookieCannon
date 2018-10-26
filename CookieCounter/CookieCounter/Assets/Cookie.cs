8abb386d5ebb593a29cd6bc4711ab3495a5f766d
Toni Kuosmanen <toni.t.kuosmanen@gmail.com>
2018-10-26 20:48:19 +0300

    Initial commit

---
+++ CookieCounter/CookieCounter/Assets/Cookie.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cookie : MonoBehaviour {
+
+    public float RotationSpeed;
+    private bool isRotating;
+
+	// Use this for initialization
+	void Start () {
+        isRotating = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isRotating)
+        {
+            transform.Rotate(Vector3.forward, RotationSpeed);
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isRotating = false;
+    }
+}
---
+++ CookieCounter/CookieCounter/Assets/CookieCannon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookieCannon : MonoBehaviour {
+
+    public GameObject CookiePrefab;
+    public float BlastForce;
+    public Transform SpawnPoint;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var axis = Input.GetAxis("Horizontal");
+        gameObject.transform.Rotate(Vector3.forward, axis * 1.0f);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var cookie = Instantiate(CookiePrefab, SpawnPoint.position, Quaternion.identity);
+            var rb = cookie.GetComponent<Rigidbody2D>();
+            Vector3 jitter = new Vector3() { x = Random.Range(-0.5f, 0.5f), y = Random.Range(-0.5f, 0.5f) };
+            Vector3 direction = Vector3.Normalize(SpawnPoint.position - transform.position + jitter );
+            rb.AddForce(direction*BlastForce);
+        }
+	}
+
+
+}
